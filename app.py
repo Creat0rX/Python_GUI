fc22f2c0d5033cdd077fa0ff78d03f9229aa0899
Creat0rX <ameen.hartley@gmail.com>
2021-10-19 21:59:42 +0200

    changed colours, key-bindings and mode activities

--- app.py
+++ app.py
@@ -9,12 +9,18 @@ def about():
     mbox.showinfo("About", f"Created by Ameen Hartley {chr(169)} 2021")
 
 def place_again():
-    pass
+    if not possible_bool.get():
+        new_x, new_y = x, y
+        while any([new_x in range(x, x+61), new_y in range(y, y+27)]):
+            new_x, new_y = randint(0, 740), randint(0, 570)
+        button.place(x=new_x, y=new_y)
+        
 
 app = tk.Tk()
 app.title("My First App")
 app.geometry("800x600")
 app.resizable(False, False)
+app["bg"] = "#00D0D0"
 
 possible_bool = tk.BooleanVar()
 possible_bool.set(True)
@@ -25,15 +31,16 @@ mainmenu = tk.Menu(app)
 app['menu'] = mainmenu
 
 filemenu = tk.Menu(mainmenu, tearoff=0)
-mainmenu.add_cascade(label="File", menu=filemenu)
-mainmenu.add_command(label="About", command=about)
-filemenu.add_checkbutton(label="Possible", onvalue=1, offvalue=0, variable=possible_bool)
-filemenu.add_checkbutton(label="Impossible", onvalue=0, offvalue=1, variable=possible_bool)
+mainmenu.add_cascade(label="File", menu=filemenu, underline=0)
+mainmenu.add_command(label="About", command=about, underline=0)
+filemenu.add_checkbutton(label="Possible", onvalue=1, offvalue=0, variable=possible_bool, underline=0)
+filemenu.add_checkbutton(label="Impossible", onvalue=0, offvalue=1, variable=possible_bool, underline=0)
 filemenu.add_separator()
-filemenu.add_command(label="Quit", command=app.destroy)
+filemenu.add_command(label="Quit", command=app.destroy, underline=0, accelerator="Ctrl+Q")
 
-button = tk.Button(app, text="Click me!", command=win_msg)
+button = tk.Button(app, text="Click me!", command=win_msg, bg="hot pink", activebackground="#00FF00")
 button.bind("<Enter>", lambda dummy: place_again())
+app.bind("<Control-q>", lambda dummy: app.destroy())
 
 button.place(x=x, y=y)
 
